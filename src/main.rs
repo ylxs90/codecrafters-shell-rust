a9684b7c181047a64863456adb3d8d168d0da182
hxiao <ylxs90@gmail.com>
2024-05-26 23:35:18 +0800

    The type builtin: executable files #MG5./

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::PathBuf;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
+
+    let path = env::var("PATH").unwrap_or_default();
+    let path: Vec<PathBuf> = path.trim().split(":").map(|s| s.into()).collect();
+    // println!("{:?}", path);
+
     let built_in = vec!["echo", "exit", "type"];
+
     // Uncomment this block to pass the first stage
     loop {
         print!("$ ");
@@ -26,7 +34,16 @@ fn main() {
                 if built_in.contains(&vec[1]) {
                     println!("{} is a shell builtin", vec[1]);
                 } else {
-                    println!("{} not found", vec[1]);
+                    match find(&path, vec[1].to_string().clone()) {
+                        None => {
+                            // should be:
+                            // println!("{}: command not found", vec[1]);
+                            println!("{}: not found", vec[1]);
+                        }
+                        Some(cmd) => {
+                            println!("{} is {}", vec[1], cmd)
+                        }
+                    }
                 }
             }
             _ => {
@@ -34,4 +51,21 @@ fn main() {
             }
         }
     }
+}
+
+fn find(paths: &Vec<PathBuf>, cmd: String) -> Option<String> {
+    for path in paths {
+        let mut path = path.clone();
+        path.push(cmd.as_str());
+        if path.is_file() {
+            return Some(path.to_str().unwrap().to_string());
+        }
+    }
+    None
+}
+
+#[test]
+fn test_find() {
+    let paths:Vec<PathBuf> = vec!["/bin"].iter().map(|s| s.into()).collect();
+    println!("{:?}", find(&paths, "cat".to_string()));
 }

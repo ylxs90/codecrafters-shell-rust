7b6d202c89b7a2d6de8f653d9da4251c75b9a5b2
hxiao <ylxs90@gmail.com>
2025-12-14 18:16:05 +0800

    update rust version

--- your_shell.sh
+++ your_shell.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
 #
-# Use this script to run your program LOCALLY.
+# DON'T EDIT THIS!
 #
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+# CodeCrafters uses this file to test your code. Don't make any changes here!
 #
-# Learn more: https://codecrafters.io/program-interface
-
-set -e # Exit early if any commands fail
-
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cargo build --release --target-dir=/tmp/codecrafters-build-shell-rust --manifest-path Cargo.toml
-)
-
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
-exec /tmp/codecrafters-build-shell-rust/release/codecrafters-shell "$@"
+# DON'T EDIT THIS!
+exec cargo run \
+    --quiet \
+    --release \
+    --target-dir=/tmp/codecrafters-shell-target \
+    --manifest-path $(dirname $0)/Cargo.toml -- "$@"
